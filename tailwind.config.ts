3727fcdada364bc7b0dada1d2a4b30b2ba5deae6
Fayaz Ahmed <fayazara@gmail.com>
2024-04-15 19:45:08 +0530

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
+  theme: {
+    fontFamily: {
+      sans: [
+        "DM Sans",
+        "Avenir Next",
+        "Roboto",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        "Ubuntu",
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      mono: [
+        "Cascadia Code",
+        "ui-monospace",
+        "SFMono-Regular",
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        "Liberation Mono",
+        "Courier New",
+        "monospace",
+      ],
+      display: ["DM Serif Display", "Inter", "sans-serif"],
+    },
+  },
+};
